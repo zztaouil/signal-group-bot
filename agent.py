03028a0c75d30af38d7ec3c110821b0f396d714e
zztaouil <ztaouil@outlook.com>
2025-06-19 14:20:20 +0000

    implement a new download_shrek function. Update bot to utilize the new download_shrek feature and adjust response timing logic.

--- agent.py
+++ agent.py
@@ -1,11 +1,15 @@
 from agno.agent import Agent
 from agno.models.anthropic import Claude
 from agno.models.openai import OpenAIChat
-
+from agno.tools.wikipedia import WikipediaTools
+from agno.tools.newspaper import NewspaperTools
+import random
+import requests
 
 def get_agent(description, instructions, expected_output):
     agent = Agent(
         model=OpenAIChat(id="gpt-4o-mini"),
+        tools=[NewspaperTools()],
         description=description,
         instructions=instructions,
         expected_output=expected_output,
@@ -14,19 +18,40 @@ def get_agent(description, instructions, expected_output):
         # warnings=False,
         read_chat_history=True,
         add_history_to_messages=True,
-        
+        num_history_responses=10,
     )
     return agent
 
 
 def generate_response(message):
+    # agent = get_agent(
+    #     description="You're an angry moroccan adult male.",
+    #     instructions="Always respond in Moroccan dialect, no arabic letters, no translation. Don't offer assistance, just respond to the message Don't ask questions in your reponse. Use vulgar darija language when appropriate. Don't use emojis.",
+    #     expected_output="A concise text message to the message sender that doesn't repeat previous responses",
+    # )
     agent = get_agent(
-        description="You're moroccan adult male.",
-        instructions="Always respond in Moroccan dialect, no arabic letters, no translation. Don't offer assistance, just respond to the message. Review the conversation history to avoid repeating yourself. Provide fresh and unique responses each time while maintaining a consistent personality. Don't ask questions in your reponse.",
-        expected_output="A concise text message to the message sender that doesn't repeat previous responses",
+        description="You're an angry recentful Moroccan male.",
+        instructions="Don't help, just respond angrily to the message. Don't use emojis. Use insults.",
+        expected_output="",
     )
-    response = agent.run(f"respond to the following message: {message}")
+    response = agent.run(f"{message}")
     return response.content
+    # possibilities = [
+    #     "kayna awda hadak",
+    #     "n3al chitan",
+    #     "fchkel hadchi",
+    #     "blan",
+    #     "ghyrha asahbi",
+    #     "ok",
+    #     "wlahila kayn",
+    #     "hdra m39ola",
+    # ]
+    # return random.choice(possibilities)
+
+def download_shrek():
+    response = requests.get("https://shreks.corneroftheinter.net/?img=png")
+    with open("shrek.png", "wb") as f:
+        f.write(response.content)
 
 if __name__ == "__main__":
     agent = get_agent(
--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import os
 from signalbot import SignalBot, Command, Context
 from dotenv import load_dotenv
 import asyncio
-from agent import generate_response
+from agent import generate_response, download_shrek
 from pprint import pprint
 import random
+import base64
 import time
-
+import requests
 
 class PingCommand(Command):
     def __init__(self):
@@ -21,22 +22,27 @@ class PingCommand(Command):
         # pprint(f"type: {c.message.type}")
         # pprint(f"text: {c.message.text}")
         # pprint(f"group: {c.message.group}")
-        if c.message.text is not None:
+        if c.message.text is not None and c.message.text.startswith("@shrek"):
             current_time = time.time()
-            should_respond = self.latest_response_time is None or current_time - self.latest_response_time >= 10
+            should_respond = self.latest_response_time is None or current_time - self.latest_response_time >= 0.1
             print(f"Time since last response: {current_time - self.latest_response_time if self.latest_response_time else 'Never'} seconds")
             print(f"Should respond: {should_respond}")
             if not should_respond:
                 return
             self.latest_response_time = current_time
             print(f"latest_response_time: {self.latest_response_time}")
-            if random.random() < 0.1:  # 10% chance
+            if random.random() < 0.5:  # 50% chance
                 await c.react("👍")
             await c.start_typing()
             await asyncio.sleep(random.randint(1, 2))
-            response = generate_response(c.message.text)
+            # response = generate_response(c.message.text)
+            download_shrek()
             await c.stop_typing()
-            await c.send(response)
+            r = requests.get("https://zenquotes.io/api/random")
+            data = r.json()
+            caption = generate_response(c.message.text.replace("@shrek", ""))
+            # caption = data[0]["q"]
+            await c.send("@pp " + caption, base64_attachments=[base64.b64encode(open("shrek.png", "rb").read()).decode("utf-8")])
 
 if __name__ == "__main__":
     load_dotenv()
